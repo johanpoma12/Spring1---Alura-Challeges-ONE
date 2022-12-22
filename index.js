b355be6052252a18f9515108e00b47fa27073374
Johan Poma <johan120402@gmail.com>
2022-12-22 18:41:12 -0500

    Terminar codigo js convert

--- index.js
+++ index.js
@@ -86,5 +86,54 @@ function encriptar(){
 
 //---------------------------------------------------------------------------
 
+function desincriptar(){
+    extra.innerHTML='';  
+
+        
+    if(text.value == ""){
+        const div = document.createElement('DIV');
+        const textwarning = `<img src="img/warning.jpg" alt="" class="muneco" style="left= 4cm">`
+        div.innerHTML= textwarning;
+        extra.appendChild(div)   
+    }
+    if(text.value !=''){
+        let tex=text.value;
+        var textoE=tex.replace(/ai/img, "a");  //i mayus y minus //g toda la linea o la oracio //m tome en cuenta multiples lineas (enter)
+        var textoE=textoE.replace(/enter/img, "e");
+        var textoE=textoE.replace(/imes/img, "i");
+        var textoE=textoE.replace(/ober/img, "o");
+        var textoE=textoE.replace(/ufat/img, "u");
+
+        const div = document.createElement('DIV')
+        const textwarning = `<div class="divArea">
+        <textarea name="area_de_texto" rows="3" cols="50" class="textoconvert">${textoE}</textarea>
+        <div class="divBoton">
+        <button class="botonCopy">Copiar texto</button>
+        </div>
+       </div>
+       </div>`
+        div.innerHTML = textwarning;
+        extra.appendChild(div)
+
+
+        const textoconvert = document.querySelector('.textoconvert');
+        const  botoncopy = document.querySelector('.botonCopy')
+        botoncopy.addEventListener('click',()=>{
+            textoconvert.select()
+            let copia = document.execCommand('copy')
+            div.innerHTML=`<div class="divArea">
+            <textarea name="area_de_texto" rows="3" cols="50" class="textoconvert">Texto copiado</textarea>
+            <div class="divBoton">
+            <button class="botonCopy">Copiar texto</button>
+           </div>
+           </div>
+          </div>`;
+    
+        extra.appendChild(div)
+        })
+    }
+}
+
 
 botonE.addEventListener('click',encriptar)
+botonD.addEventListener('click',desincriptar)
